ca313d92fc764939b61580b1a26ce6d33905ec3d
coyzeng <coyzeng@gmail.com>
2021-04-21 23:57:47 +0800

    project structure

---
+++ prince-javascript/src/main/java/com/ducesoft/babel/javascript/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2000, 2099, ducesoft and/or its affiliates. All rights reserved.
+ * DUCESOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *//**
+ * @author coyzeng@gmail.com
+ */
+package com.ducesoft.babel.javascript;
