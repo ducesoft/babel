79579ce19b2ac2a0008e48eeb33972e4b89b34aa
coyzeng <coyzeng@gmail.com>
2021-05-02 00:26:51 +0800

    spi Interpreter

---
+++ emperor/src/main/java/com/ducesoft/babel/emperor/Interpreter.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2000, 2099, ducesoft and/or its affiliates. All rights reserved.
+ * DUCESOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ */
+package com.ducesoft.babel.emperor;
+
+/**
+ * @author coyzeng@gmail.com
+ */
+public interface Interpreter {}
---
+++ emperor/src/main/java/com/ducesoft/babel/emperor/struct/Parameter.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2000, 2099, ducesoft and/or its affiliates. All rights reserved.
+ * DUCESOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ */
+package com.ducesoft.babel.emperor.struct;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * @author coyzeng@gmail.com
+ */
+@Data
+public class Parameter implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -1805025005051265107L;
+}
